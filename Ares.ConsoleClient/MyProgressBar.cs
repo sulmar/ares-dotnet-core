5a8cd8aa8e7368864241dbf77682de4961bfa042
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2020-05-28 15:08:57 +0200

    progress i anulowanie tasków

---
+++ Ares.ConsoleClient/MyProgressBar.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ares.ConsoleClient
+{
+    public class MyProgressBar : IProgress<int>
+    {
+        public void Report(int value)
+        {
+            Console.WriteLine(value);
+
+           // progressBar.Value = value;
+        }
+    }
+}
--- Ares.ConsoleClient/Program.cs
+++ Ares.ConsoleClient/Program.cs
@@ -3,25 +3,242 @@ using Ares.Infrastructure.Fakers;
 using Ares.Infrastructure.FakeServices;
 using Microsoft.Extensions.Options;
 using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Ares.ConsoleClient
 {
     class Program
     {
+        static void Worker(CancellationToken cancellationToken = default)
+        {
+            while(!cancellationToken.IsCancellationRequested)
+            {
+                Console.Write(".");
+                Thread.Sleep(TimeSpan.FromSeconds(0.5));
+            }
+        }
+
+        static void Downloads(
+            CancellationToken cancellationToken = default,
+            IProgress<int> progress = null)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                if (cancellationToken.IsCancellationRequested)
+                {
+                    return;
+                }
+
+                // cancellationToken.ThrowIfCancellationRequested();
+
+                // Console.Write(".");
+
+                progress?.Report(i);
+
+                Thread.Sleep(TimeSpan.FromSeconds(0.5));
+            }
+        }
+
+        public static Task DownloadsAsync(
+                CancellationToken cancellationToken = default,
+                IProgress<int> progress = null)
+        {
+            return Task.Run(() => Downloads(cancellationToken, progress));
+        }
+
+        private static void DownloadsTest()
+        {
+            CancellationTokenSource cts = new CancellationTokenSource();
+
+            CancellationToken cancellationToken = cts.Token;
+
+          //  IProgress<int> progress = new Progress<int>(step => Console.Write("."));
+
+            IProgress<int> progress = new MyProgressBar();
+
+            DownloadsAsync(cancellationToken, progress);
+
+
+
+            Console.WriteLine("Press Enter key to cancel.");
+            Console.ReadLine();
+
+            cts.Cancel();
+        }
+
+        private static void DownloadsTimerTest()
+        {
+            CancellationTokenSource cts = new CancellationTokenSource();
+
+            CancellationToken cancellationToken = cts.Token;
+
+            cts.CancelAfter(TimeSpan.FromSeconds(5));
+
+            DownloadsAsync(cancellationToken);
+        }
+
+        private static void DownloadsTimerTest2()
+        {
+            CancellationTokenSource cts = new CancellationTokenSource(TimeSpan.FromSeconds(5));
+
+            CancellationToken cancellationToken = cts.Token;
+
+            DownloadsAsync(cancellationToken);
+
+        }
+
+
+
+        static void DoWork()
+        {
+            Console.WriteLine($"#{Thread.CurrentThread.ManagedThreadId} working...");
+
+            Thread.Sleep(TimeSpan.FromSeconds(5));
+
+            Console.WriteLine($"#{Thread.CurrentThread.ManagedThreadId} Done.");
+        }
+
+
+        
+
+        static decimal Calculate(int length)
+        {
+            Console.WriteLine($"#{Thread.CurrentThread.ManagedThreadId} calculating {length}...");
+           
+            Thread.Sleep(TimeSpan.FromMilliseconds(length));
+
+            decimal cost = length * 0.99m;
+
+            Console.WriteLine($"#{Thread.CurrentThread.ManagedThreadId} calculated {cost}...");
+
+            return cost;
+
+
+        }
+
+       
+
+        static void DownloadTaskTest()
+        {
+            //Task<int> task = Task.Run(() => Download("http://www.sulmar.pl"));
+            //task.ContinueWith(t => Console.WriteLine(t.Result));
+
+            Task.Run(() => Download("http://www.sulmar.pl"))
+                .ContinueWith(t => Task.Run(() => Calculate(t.Result)))
+                    .ContinueWith(t => Console.WriteLine($"Cost: {t.Result.Result}"));
+
+        }
+
+        static int Download(string url)
+        {
+            Console.WriteLine($"#{Thread.CurrentThread.ManagedThreadId} downloading {url}...");
+
+            var client = new WebClient();
+            string content = client.DownloadString(url);
+            Thread.Sleep(TimeSpan.FromSeconds(5));
+
+            Console.WriteLine($"#{Thread.CurrentThread.ManagedThreadId} downloaded {url}.");
+
+            return content.Length;
+        }
+
+        static Task<int> DownloadAsync(string url)
+        {
+            return Task.Run(() => Download(url));
+        }
+
+        static Task<decimal> CalculateAsync(int length)
+        {
+            return Task.Run(() => Calculate(length));
+        }
+
+        static void DownloadContinueWithTest()
+        {
+            DownloadAsync("http://www.sulmar.pl")
+                .ContinueWith(t => CalculateAsync(t.Result))
+                    .ContinueWith(t => Console.WriteLine($"Cost: {t.Result.Result}"));
+        }
+
+        static void DownloadSyncTest()
+        {
+            int length = Download("http://www.sulmar.pl");
+            decimal cost = Calculate(length);
+            Console.WriteLine($"Cost: {cost}");
+        }
+
+        static async void DownloadAsyncAwaitTest()
+        {
+            int length = await DownloadAsync("http://www.sulmar.pl").ConfigureAwait(false);
+            decimal cost = await CalculateAsync(length);
+            Console.WriteLine($"Cost: {cost}");
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine($"#{Thread.CurrentThread.ManagedThreadId} Hello World!");
+
+            DownloadsTest();
+
+           // DownloadsTimerTest();
+
+            //Task task1 = DownloadAsync("http://www.sulmar.pl");
+
+            //Task task2 = DownloadAsync("http://www.microsoft.com");
+
+            // Task.WaitAll(task1, task2);
+
+            // Task.WhenAny(task1, task2);
 
 
-            GetProductsTest();
+
+
+
+            //Thread thread = new Thread(() => DoWork());
+            //thread.Start();
+
+            // Task task = new Task(() => DoWork());
+
+            // Task task=  Task.Run(() => DoWork());
+
+
+
+            // Task.Run(() => Download("http://www.sulmar.pl"));
+
+            // DownloadSyncTest();
+            //DownloadTaskTest();
+
+            // DownloadAsyncAwaitTest();
+
+
+
+
+            //ICollection<Task> tasks = new List<Task>();
+
+            //tasks.Add(new Task(() => DoWork()));
+            //tasks.Add(new Task(() => DoWork()));
+            //tasks.Add(new Task(() => DoWork()));
+
+            //foreach (var t in tasks)
+            //{
+            //    t.Start();
+            //}
+
+            //  task.Start();
+
+
+
+            Console.WriteLine($"#{Thread.CurrentThread.ManagedThreadId} Press any key to exit.");
+            Console.ReadKey();
+
+            // GetProductsTest();
 
             // ExtentionMethodTests.Test();
 
-           // GenericTypesTests.Test();
-
-            Console.WriteLine("Press any key to exit.");
-            Console.ReadKey();
-
+            // GenericTypesTests.Test();
 
             //Phone phone = new Phone();
             //phone.Call("555-556-444", "Lorem");
