59638c779ee0558b796a34bef017255344e9c36f
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2020-06-22 12:27:21 +0200

    Message

---
+++ Ares.Domain/Models/Message.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ares.Domain.Models
+{
+    public class Message
+    {
+        public string Title { get; set; }
+        public string Contentss { get; set; }
+    }
+}
